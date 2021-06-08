ff7f44ebaed801b2c61a5b1cba3e073c6cacd5dd
PhanindraBabuPothuru <80087361+PhanindraBabuPothuru@users.noreply.github.com>
2021-06-08 10:42:54 +0530

    starProgram.py

---
+++ pythonstar.py
@@ -0,0 +1,11 @@
+v_Input = int(input("Enter Number of stars you want"))
+
+i=1
+
+while (i<=v_Input):
+    j=1
+    while (j<=v_Input):
+        print('*',end='')
+        j=j+1
+    i=i+1
+    print()
